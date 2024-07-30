4c384026197c9b8cb7388768cc9418161a3d9428
rk8713786 <rjm9871@gmail.com>
2024-07-31 04:08:36 +0530

    Corrected

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const twilio = require('twilio');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv').config();
 
 
 const app = express();
@@ -16,23 +17,23 @@ app.use(cors());
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'airlinessystem821@gmail.com', // Replace with your email
-    pass: 'kzxs fkwe rhwf vbgd'   // Replace with your email password
+    user: process.env.REACT_APP_USER, // Replace with your email
+    pass: process.env.REACT_APP_PASS   // Replace with your email password
   }
 });
 
 // Twilio configuration
-const accountSid = 'AC2dab0eebb4483c7509cd9d6244af138c'; // Replace with your Twilio Account SID
-const authToken = 'e77813b665ecd72fba485a1f088ca8c1';   // Replace with your Twilio Auth Token
+const accountSid = process.env.REACT_APP_ACCOUNT_SID; // Replace with your Twilio Account SID
+const authToken = process.env.REACT_APP_AUTH_TOKEN;   // Replace with your Twilio Auth Token
 const client = twilio(accountSid, authToken);
 
 // Route to send email
 app.post('/send-email', async (req, res) => {
   const { to, subject, text } = req.body;
-
+console.log(req.body)
   try {
     const info = await transporter.sendMail({
-      from: 'rjm9871@gmail.com', // Replace with your email
+      from: process.env.REACT_APP_USER, // Replace with your email
       to:'rk99576@gmail.com',
       subject:"Testing",
       text:"Hello"
@@ -46,12 +47,13 @@ app.post('/send-email', async (req, res) => {
 
 // Route to send SMS
 app.post('/send-sms', async (req, res) => {
+  console.log(req.body)
   const { to, body } = req.body;
-
+  console.log(req.body)
   try {
     const message = await client.messages.create({
       body: "Hi Rahul",
-      from: '+16187624806', // Replace with your Twilio phone number
+      from: process.env.REACT_APP_FROM_MOB_NO, // Replace with your Twilio phone number
       to: '+919911069251'
     });
     res.status(200).send(`Message sent successfully: ${message.sid}`);
